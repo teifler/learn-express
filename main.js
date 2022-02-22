416f9421a641f069f41b4199ddd9d5840b738154
Tim Eifler <tim.eif@protonmail.com>
2022-02-22 21:22:43 +0100

    Add form and more middelware functions

--- main.js
+++ main.js
@@ -1,22 +1,47 @@
 const express = require('express')
+const res = require('express/lib/response')
 const app = express()
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 //middleware - - looks at body of request
+//use means for all middleware / functions
 app.use(express.urlencoded({ extended: false }))
 
 //request //response
 //Load Website is get request
-app.get('/', (req, res) => {
+
+//End of the function call next --> so they know this middle
+//ware function has done its job ggo to next function
+function getWeather(req, res, next) {
+  req.visitorWeather = false
+  req.visitorWeather === true
+    ? res.send('Please come back when its not raining')
+    : next()
+}
+
+//route to /
+//we can define functions for routes as many as we want
+
+app.get('/', getWeather, (req, res) => {
   res.send(`
   <h1?>What color is the sky on a clear day?</h1?>
   <form action="/result" method="POST">
     <input type="text" name="color">
     <button>Submit Answer</button>
-  </form>`)
+  </form>
+  <p>${req.visitorWeather ? 'It is raining' : 'It is not raining'}</p>
+  `)
 })
 
 app.get('/about', (req, res) => {
   res.send('Thanks for learning more about us')
+})
+
+app.get('/crypto', (req, res) => {
+  res.send('Bitcoin to the moon')
 })
 
 app.post('/result', (req, res) => {
